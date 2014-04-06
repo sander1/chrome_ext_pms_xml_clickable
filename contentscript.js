08688015ca7a5bd8c7eefd22725feb42660424ae
sander1 <sander@plexapp.com>
2014-04-07 01:03:41 +0200

    first commit

---
+++ contentscript.js
@@ -0,0 +1,25 @@
+var attributes = document.getElementsByClassName('webkit-html-attribute-value');
+
+for (var i=0; i<attributes.length; i++)
+{
+
+	if (attributes[i].childNodes.length === 0)
+	{
+		continue;
+	}
+
+	var node = attributes[i].childNodes[0];
+
+	if (node.nodeValue.substring(0, 7) == 'http://' || node.nodeValue.substring(0, 1) == '/')
+	{
+		var url = node.nodeValue;
+		var textNode = document.createTextNode(url);
+
+		var a = document.createElement('a');
+		a.setAttribute('href', url);
+		a.addEventListener('click', function() { window.location = this.getAttribute('href'); }, false);
+		a.appendChild(textNode);
+
+		node.parentNode.replaceChild(a, node);
+	}
+}
